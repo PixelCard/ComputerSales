55b522960b445d6cf49a65d8f5b880b10b4084a4
Nguyen-Truong-Quy <Nguyentruongquy76@gmail.com>
2025-09-13 14:46:13 +0700

    Done Order , Customer

---
+++ Project_AppllicationComputer/ComputerProject/ComputerSales.Application/UseCaseDTO/ProductNews_DTO/ProductNewsInputDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComputerSales.Application.UseCaseDTO.ProductNews_DTO
+{
+    public sealed record ProductNewsInputDTO
+    (
+      string BlockType,
+      string? TextContent ,
+      string? ImageUrl ,
+      string? Caption ,
+      int DisplayOrder ,
+      DateTime CreateDate
+    );
+  
+}
---
+++ Project_AppllicationComputer/ComputerProject/ComputerSales.Domain/Entity/E_ProductNews/ProductNews.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ComputerSales.Domain.Entity.ENews
+{
+    public class ProductNews
+    {
+        [Column("ProductNewsID")]
+        public int ProductNewsID { get; set; }
+
+        public string BlockType { get; set; } = default!; // ENUM text, list, image, logo, table
+        public string? TextContent { get; set; }
+        public string? ImageUrl { get; set; }
+        public string? Caption { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreateDate { get; set; }
+
+        // Factory method để tạo nhanh entity
+        public static ProductNews Create(
+            string blockType,
+            string? textContent,
+            string? imageUrl,
+            string? caption,
+            int displayOrder,
+            DateTime createDate)
+        {
+            return new ProductNews
+            {
+                BlockType = blockType,
+                TextContent = textContent,
+                ImageUrl = imageUrl,
+                Caption = caption,
+                DisplayOrder = displayOrder,
+                CreateDate = createDate
+            };
+        }
+    }
+}
